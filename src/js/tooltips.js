0844291f7897c9dcfcc6a45495a19c8d4416327a
Gian <giangougeon@gmail.com>
2025-08-17 15:38:29 -0300

    [15/08/2025] (15:37)  | Merge con santiago y sumando el html de noticiasYLanzamientos, y mejoras de accesibilidad

--- src/js/tooltips.js
+++ src/js/tooltips.js
@@ -2,4 +2,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const tooltips = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     tooltips.map((t) => new bootstrap.Tooltip(t));
+    // Activar popovers
+    const popovers = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    popovers.map((p) => new bootstrap.Popover(p));
 });
+
